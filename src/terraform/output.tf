80ecb8664f13dc999b883b68f0f55fc6aef01e8c
Dino Wang <dino.cool@gmail.com>
2025-11-18 16:52:32 +0800

    terraform output refactor

--- src/terraform/output.tf
+++ src/terraform/output.tf
@@ -7,3 +7,13 @@ output "zipdeploy_url" {
   description = "The URL to use for Zip Deployments to the App Service"
   value       = "https://web-${var.codename}-${random_id.codename_suffix.hex}.scm.azurewebsites.net/ZipDeployUI"
 }
+
+output "resource_group" {
+  description = "The name of the Resource Group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "app_name" {
+  description = "The name of the App Service"
+  value       = "web-${var.codename}-${random_id.codename_suffix.hex}"
+}
